d1f68e1ee14f85d475a780c864f40e31192eb811
Ethan Chen <ktun7@mail.ccsf.edu>
2017-01-06 14:25:53 -0800

    First Commit

--- tipassasin/ViewController.swift
+++ tipassasin/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  tipassasin
 //
-//  Created by Ethan Chen on 1/2/17.
+//  Created by Ethan Chen on 1/2/17.œ
 //  Copyright © 2017 kyaw tun. All rights reserved.
 //
 
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func onTap(_ sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text =  String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+   
 }
 
