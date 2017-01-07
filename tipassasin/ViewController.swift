2d47937b6eddc0ca9696b37d5e19843445e8ebc3
Ethan Chen <ktun7@mail.ccsf.edu>
2017-01-06 21:27:26 -0800

    Fixed the ReadMe

--- tipassasin/ViewController.swift
+++ tipassasin/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.title = "Tip Calculator"
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,6 +43,6 @@ class ViewController: UIViewController {
         tipLabel.text =  String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
     }
-   
+      
 }
 
